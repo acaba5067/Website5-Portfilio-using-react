28327bd679278e1f2a686629e80e08ca0a716653
acaba5067 <acaba5067@gmail.com>
2024-09-07 20:54:20 +0300

    b

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -31,7 +31,7 @@ export const Newsletter = ({ status, message, onValidated }) => {
               {status === 'error' && <Alert variant="danger">{message}</Alert>}
               {status === 'success' && <Alert variant="success">{message}</Alert>}
             </Col>
-            <Col md={12} xl={7}>
+            <Col md={6} xl={7}>
               <form onSubmit={handleSubmit}>
                 <div className="new-email-bx">
                   <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder=" Enter Email Address" />
